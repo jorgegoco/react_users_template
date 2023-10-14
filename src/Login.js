2260e37346c8c9c1459f408656c10f47ea9aec49
jorgegoco <jorgegoco70@gmail.com>
2023-10-14 20:15:41 +0200

    Set up axios to log in in server

--- src/Login.js
+++ src/Login.js
@@ -23,8 +23,35 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+
     try {
-    } catch (err) {}
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            // withCredentials: true,
+          },
+        }
+      )
+      const accessToken = response?.data?.accessToken
+      const roles = response?.data?.roles
+      setAuth({ user, pwd, roles, accessToken })
+      setUser("")
+      setPwd("")
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response")
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password")
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized")
+      } else {
+        setErrMsg("Login Failed")
+      }
+      errRef.current.focus()
+    }
   }
 
   return (
