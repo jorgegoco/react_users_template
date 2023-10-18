6949da29dc148727a5301314864842d44e1ed7d5
jorgegoco <jorgegoco70@gmail.com>
2023-10-18 21:42:26 +0200

    Add persist login functionality with checkbox and localStorage

--- src/components/PersistLogin.js
+++ src/components/PersistLogin.js
@@ -5,20 +5,24 @@ import useAuth from "../hooks/useAuth"
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true)
   const refresh = useRefreshToken()
-  const { auth } = useAuth()
+  const { auth, persist } = useAuth()
 
   useEffect(() => {
+    let isMounted = true
+
     const verifyRefreshToken = async () => {
       try {
         await refresh()
       } catch (err) {
         console.error(err)
       } finally {
-        setIsLoading(false)
+        isMounted && setIsLoading(false)
       }
     }
 
     !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false)
+
+    return () => (isMounted = false) // Clean up function
   }, [])
 
   useEffect(() => {
@@ -26,7 +30,9 @@ const PersistLogin = () => {
     console.log(`aT: ${JSON.stringify(auth?.accessToken)}`)
   }, [isLoading])
 
-  return <>{isLoading ? <p>Loading...</p> : <Outlet />}</>
+  return (
+    <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
+  )
 }
 
 export default PersistLogin
